dd53a868b29530b50428176991e3fefee9fcdc49
settingcons <settingcons@gmail.com>
2015-09-04 12:03:03 +0200

    20150904_26

--- www/js/index.js
+++ www/js/index.js
@@ -48,16 +48,16 @@ function Reproducir1(){
 
 function Reproducir(){
     alert('Reproducir');
-    alert(window.location.href);
+    //alert(window.location.href);
     //var v_fichero = ObtenerFicheroAudio();
     var v_fichero = _mediaAudioFicheroIOS;
 
-    //var v_dir=window.rootFS.toURL();
-    //v_dir=v_dir.substring("file://".length);
-    var v_dir =window.location.href;
+    var v_dir=window.rootFS.toURL();
     v_dir=v_dir.substring("file://".length);
-    v_dir = v_dir.substring(0, v_dir.indexOf("/VozEfectoTEST.app/www/index.html"))
-    v_dir=v_dir+"/Documents/";
+    //var v_dir =window.location.href;
+    //v_dir=v_dir.substring("file://".length);
+    //v_dir = v_dir.substring(0, v_dir.indexOf("/VozEfectoTEST.app/www/index.html"))
+    //v_dir=v_dir+"/Documents/";
     alert(v_dir);
     v_fichero=v_dir+v_fichero;
     loadSound(v_fichero);
@@ -85,6 +85,7 @@ function loadSound(url) {
         alert('loadSound1');
         request.onload = function() {
             alert('loadSound2');
+            alert(request.response);
             try
             {
                 context.decodeAudioData(request.response, function(buffer) {
