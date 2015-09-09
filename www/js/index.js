e381fe3b6e4733d4e5a7c531add1f1b0187ed72c
settingcons <settingcons@gmail.com>
2015-09-09 13:40:10 +0200

    20150909_18

--- www/js/index.js
+++ www/js/index.js
@@ -90,12 +90,7 @@ var miFichero=null;
 function Reproducir_MVL() {
     try {
         var v_fichero = '';
-        limpiarMedia();
-
-        //if (audio) audio.remove();
-        //if (sourceNode) sourceNode.disconnect();
-        //cancelAnimationFrame(audioAnimation);
-        //audio = new Audio();
+        //limpiarMedia();
 
         if(esIOS()) {
             v_fichero = _mediaAudioFicheroIOS;
@@ -110,16 +105,15 @@ function Reproducir_MVL() {
         }
         //audio.src = v_fichero;
         miFichero = v_fichero;
-        setupAudioNodes_MVL();
+
+        try{
+
+            loadSoundTestFreq(miFichero); //audioFile
+        }
+        catch (ex){alert('ERROR. Exception: '+ex.message);}
+
     }
     catch (ex){mensaje(ex.message,'loadSong');}
-}
-
-function setupAudioNodes_MVL() {
-    try {
-        loadSound_MVL(miFichero);
-    }
-    catch (ex){mensaje(ex.message,'setupAudioNodes');}
 }
 
 function setupAudioNodes() {
@@ -181,38 +175,6 @@ function loadSound(url) {
     catch (ex){alert('loadSound: '+ex.message);}
 }
 
-function loadSound_MVL(url) {
-    try {
-        alert(url);
-
-        limpiarMedia();
-
-
-
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.responseType = 'arraybuffer';
-
-        alert('loadSound1');
-        request.onload = function() {
-            alert('loadSound2');
-            alert(request.response);
-            try
-            {
-                context.decodeAudioData(request.response, function(buffer) {
-                    alert('ok');
-                    //sound = buffer;
-                    playSound_MVL(buffer);
-                },ErrorLoad);
-            }
-            catch (ex){mensaje(ex.message,"ERROR on load")}
-        }
-
-        request.send();
-    }
-    catch (ex){alert('loadSound: '+ex.message);}
-}
-
 function limpiarMedia()
 {
     try {
@@ -242,68 +204,6 @@ function playSound(buffer) {
         source.buffer = buffer;
         source.connect(context.destination);
         source.start(0);
-        alert('playSound fin');
-    }
-    catch (ex){mensaje(ex.message,'ERROR playSound');}
-}
-
-
-function playSound_MVL(buffer) {
-    try {
-        alert('playSound');
-        source = context.createBufferSource();
-        source.buffer = buffer;
-
-        /* ------------------------------------------------------------------*/
-        // MVL - 09.09.2015 --> aplicar efectos
-        //applyEffects();
-
-        if (document.getElementById('Speed_chk').checked){
-            source.playbackRate.value = rangeSP_Speed_lbl.innerHTML;
-        }
-
-        //var compressor = context.createDynamicsCompressor();
-        /*
-         var compressor = (compressor|| context.createDynamicsCompressor());
-         if (document.getElementById('Compressor_chk').checked) {
-         compressor.threshold.value = rangeCMP_threshold_lbl.innerHTML;
-         compressor.knee.value = rangeCMP_knee_lbl.innerHTML;
-         compressor.ratio.value = rangeCMP_ratio_lbl.innerHTML;
-         compressor.reduction.value = rangeCMP_reduction_lbl.innerHTML;
-         compressor.attack.value = rangeCMP_attack_lbl.innerHTML;
-         compressor.release.value = rangeCMP_release_lbl.innerHTML;
-         }
-         */
-
-
-        //var biquadFilter = context.createBiquadFilter();
-        //biquadFilter = (biquadFilter || context.createBiquadFilter());
-/*
-         var biquadFilter = context.createBiquadFilter();
-         if (document.getElementById('BiquadFilter_chk').checked){
-             // Manipulate the Biquad filter
-             // Type : lowshelf, highshelf, peaking
-             biquadFilter.type = document.getElementById('rangeBQ_type').value;  //"peaking";
-             alert('BQ Type : ' + biquadFilter.type);
-             biquadFilter.frequency.value = rangeBQ_freq_lbl.innerHTML;          //350;    //aprox. frequency human 350
-             biquadFilter.gain.value = rangeBQ_gain_lbl.innerHTML;               //25
-             biquadFilter.Q.value = rangeBQ_Qf_lbl.innerHTML;                    // nominal range of 0.0001 to 1000.
-             biquadFilter.detune.value = rangeBQ_detune_lbl.innerHTML;           //1540;
-         }
-
-         source.connect(biquadFilter);
-         biquadFilter.connect(context.destination);
-*/
-
-        //source.connect(context.destination);
-        if(esIOS()){
-            source.noteOn(0);
-            alert('noteOn');
-        }
-        else{
-            source.start(0);
-        }
-
         alert('playSound fin');
     }
     catch (ex){mensaje(ex.message,'ERROR playSound');}
