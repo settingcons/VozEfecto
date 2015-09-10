1cf0ab0219bf5dfe66b7faa6efefd8deae77e024
settingcons <settingcons@gmail.com>
2015-09-10 12:14:31 +0200

    20150910_04

--- www/js/share_files.js
+++ www/js/share_files.js
@@ -5,8 +5,72 @@
 function shareFiles(){
     alert('entra shareFiles');
     try{
-        app.openExternalDoc();
+        //app.openExternalDoc();
+        navigator.openExternalDoc();
         alert('Después openExternalDoc');
     }
-    catch (ex){mensaje(ex.message,'ERROR playSound');}
+    catch (ex){mensaje('shareFiles. Error: ' + ex.message,'ERROR Sharing');}
+}
+
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/* ---- PLUGIN -- EddyVerbruggen/SocialSharing-PhoneGap-Plugin -------------- */
+/* -------------------------------------------------------------------------- */
+/* https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin */
+
+function SocialSharing(){
+
+    alert('entra SocialSharing');
+    try {
+        var v_fichero = '';
+        if(esIOS()) {
+            v_fichero = _mediaAudioFicheroIOS;
+            var v_dir = window.rootFS.toURL();
+            v_dir = v_dir.substring("file://".length);
+            v_fichero = v_dir + v_fichero;
+        }
+        else{
+            v_fichero=ObtenerFicheroAudio();
+        }
+
+        miFichero = v_fichero;
+
+        window.plugins.socialsharing.share('Here is your WAV file', 'Your WAV', miFichero);
+        alert('Después ejecutar SocialSharing');
+
+    }
+    catch (ex){mensaje(ex.message,'SocialSharingWhat');}
+}
+
+function SocialSharingWhatApp(){
+
+    alert('entra SocialSharingWhatApp');
+    try {
+        var v_fichero = '';
+        if(esIOS()) {
+            v_fichero = _mediaAudioFicheroIOS;
+            var v_dir = window.rootFS.toURL();
+            v_dir = v_dir.substring("file://".length);
+            v_fichero = v_dir + v_fichero;
+        }
+        else{
+            v_fichero=ObtenerFicheroAudio();
+        }
+
+        miFichero = v_fichero;
+
+        //window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp', null /* img */, null /* url */, function() {console.log('share ok')}, function(errormsg){alert(errormsg)});
+        window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp', null /* img */, null /* url */,
+                function() {alert('share ok')},
+                function(errormsg){alert(errormsg)});
+
+        alert('Después ejecutar SocialSharingWhatApp');
+
+    }
+    catch (ex){mensaje(ex.message,'SocialSharingWhatApp');}
+
 }
